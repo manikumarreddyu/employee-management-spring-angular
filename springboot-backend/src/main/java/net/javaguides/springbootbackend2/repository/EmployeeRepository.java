b79c411bf4ce7711ec8c657aad43a0093199bd65
manikumarreddyu <manikumarreddyu@gmail.com>
2023-05-14 11:55:49 +0530

    first

--- springboot-backend/src/main/java/net/javaguides/springbootbackend2/repository/EmployeeRepository.java
+++ springboot-backend/src/main/java/net/javaguides/springbootbackend2/repository/EmployeeRepository.java
@@ -1,11 +1,11 @@
 package net.javaguides.springbootbackend2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
 import net.javaguides.springbootbackend2.model.Employee;
 
-//@Repository
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
