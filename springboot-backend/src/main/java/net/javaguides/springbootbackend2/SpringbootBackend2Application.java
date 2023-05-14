1ce687c91d06811b8aa53c27637c76cf93e36655
manikumarreddyu <manikumarreddyu@gmail.com>
2023-05-14 11:22:45 +0530

    first

---
+++ springboot-backend/src/main/java/net/javaguides/springbootbackend2/SpringbootBackend2Application.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackend2Application.class, args);
+	}
+
+}
---
+++ springboot-backend/src/test/java/net/javaguides/springbootbackend2/SpringbootBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
